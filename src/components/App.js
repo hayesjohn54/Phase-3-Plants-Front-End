5e1ff4b9c55376598fd1721d707eae3ef09a122f
Spencer Mathews <smathews0906@gmail.com>
2022-08-02 15:30:08 -0500

    connecting front end and back and adding plants to plant card

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
+import { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import PlantContainer from './PlantContainer'
 
 function App() {
+ const [plants, setPlants]=useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:9292/plants')
+      .then(res=>res.json())
+      .then(plantsData => setPlants(plantsData))
+  },[])
+  console.log(plants)
   return (
     <div>
       <NavBar/>
-      <PlantContainer/>
+      <PlantContainer plants={plants}/>
     </div>
     
   );
--- src/components/PlantsCard.js
+++ src/components/PlantsCard.js
@@ -1,17 +1,17 @@
-function PlantCard(){
+function PlantCard({plant}){
     return(
         <li className="cards__item">
         <div className="card">
           <img
-            src='https://images.unsplash.com/photo-1581368976940-534956cab59b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3Jvb3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+            src={plant.image}
             alt='Groot'
             className="card__image"
           />
           <div className="card__content">
-            <div className="card__title">🌱: Groot</div>
-            <p className="card__text">Groot</p>
+            <div className="card__title">{plant.name}</div>
+            <p className="card__text">{plant.description}</p>
             <div className="card__detail">
-              <p>It's Groot</p>
+              <p>${plant.price}</p>
             </div>
           </div>
         </div>
